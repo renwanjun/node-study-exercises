5027cc555342554ff1019ef47f3cd64d57681346
renwj <994435941@qq.com>
2017-11-14 18:01:56 +0800

    更新接口

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ import connect from 'connect';
 
 const app=connect();
 app.use(logger);
-app.use('/admin',restrict);
+// app.use('/',restrict);
+app.use('/admin',restrict); // 路由
 app.use('/admin',admin);
 
 app.listen('3006',function(){
@@ -22,18 +23,21 @@ function logger(req,res,next){
 }
 function test(req,res,next){
     res.setHeader('Content-Type','text/plain');
+    res.setHeader("Access-Control-Allow-Origin", "*");  // 允许跨域
+    //告诉浏览器编码方式  
+    res.setHeader("Content-Type","text/html;charset=UTF-8" ); 
     res.end('hello world');
 }
 
 function restrict(req,res,next){
-    var authorization=req.headers.authorization;
-    if(!authorization)return next(new Error('Unauthorized'));
+    // var authorization=req.headers.authorization;
+    // if(!authorization)return next(new Error('Unauthorized'));
 
-    var parts=authorization.split('');
-    var scheme=parts[0];
-    var auth=new Buffer(parts[1],'base64').toString().split(':');
-    var user=auth[0];
-    var pass=auth[1];
+    // var parts=authorization.split('');
+    // var scheme=parts[0];
+    // var auth=new Buffer(parts[1],'base64').toString().split(':');
+    // var user=auth[0];
+    // var pass=auth[1];
     next();
     // authenticateWithDataBase(user,pass,function(err){
     //      if(err) return next(err);
@@ -43,12 +47,11 @@ function restrict(req,res,next){
 }
 function admin(req,res,next){
     switch(req.url){
-        case '/':
-        res.end('try /login');
-        break;
         case '/login':
         test(req,res,next);
         break;
+        case '/':
         default:
+        res.end('try /login');
     }
 }
