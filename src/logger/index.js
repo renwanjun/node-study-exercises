e735448bad68f549c58121f37b2e1a8e465a015d
renwj <994435941@qq.com>
2017-11-15 18:13:46 +0800

    添加日志文件夹

--- src/logger/index.js
+++ src/logger/index.js
@@ -3,6 +3,10 @@
 export default function(format){
     const regexp=/ :(\w+) /g
     return function (req,res,next){
+        // var str = format.replace(regexp,function(match,property){
+        //     return req[property];
+        // })
+
         var str = format.replace(regexp,function(match,property){
             return req[property];
         })
