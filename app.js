82204fa30b821c1b860f06cb30c87b465db0f3ca
renwj <994435941@qq.com>
2019-09-05 16:28:56 +0800

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express=require("express")
-const app=express();
- app.get('/',(req,res) => {
-    const num=100 
+const express = require("express")
+const app = express();
+app.get('/', (req, res) => {
+    const num = 100
     res.send('Hello World')
 })
- app.listen(3000,()=>console.log('程序运行在3000端口'))
+app.listen(3000, () => console.log('程序运行在3000端口'))
