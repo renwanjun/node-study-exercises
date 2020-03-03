8bc01189fdccab43e36ad24300dd9e640a285ffb
renwj <994435941@qq.com>
2020-03-03 17:24:47 +0800

    更新readme文件

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # node-end
 提供接口
-
-
 #vscode node工程调试环境配置 是node得源文件支持debug
 https://www.v2ex.com/t/391485#reply11
 https://github.com/Monsoir/Notes/blob/master/Visual%20Studio%20Code/Visual%20Studio%20Code%20%E4%B8%AD%E8%B0%83%E8%AF%95%E4%BD%BF%E7%94%A8%20ES6%20%E7%89%B9%E6%80%A7%E7%9A%84%20Node.js.md
 
 https://babeljs.io/docs/usage/cli/#babel-compile-with-source-maps
+
+## 文件目录
+
+`exercises`文件夹存放node.js练习文件
+
+## Node运行和调试
+1.以CLI的方式运行
+    
+    $ node 
+2.执行脚本文件,例如hello_world.js
+    
+    $ node hello_world.js
+
--- cmd.js
+++ cmd.js
@@ -1,7 +1,42 @@
+
+//import * as util from 'util'
+const util=require('util')
+const fs=require('fs')
+
+
+let readStream=fs.createReadStream('read.txt');
+readStream.on('data',function(data){
+    console.log('start',data.toString())
+})
+readStream.on('end',function(){
+    console.log('file ended')
+})
+
+
+//console.log(slice)
+util.log('Hello');
+let v=0;
+function a(v){
+    let v2=100;
+    v+=v2
+}
+a(v);
 const arg=process.argv;
-console.log(arg)
+//console.log(arg)
 function test(){
     this.name='22'
-    console.log(this.name)
+    //console.log(this.name)
 }
 test()
+
+// 递归函数 求阶乘!num
+function factorial(num){
+    console.log(num)
+    if(num<=1){
+        return 1
+    }else{
+        return num*factorial(num-1)
+    }
+}
+factorial(3)
+// console.log(factorial(3))
